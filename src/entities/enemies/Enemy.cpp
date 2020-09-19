b3d8326fe8ceee3b303ae291c5df211ee1ad9ce8
Connor Pietrasik <connorgpietrasik@gmail.com>
2020-09-19 05:29:26 -0700

    Made zombie rotate to follow player

--- src/entities/enemies/Enemy.cpp
+++ src/entities/enemies/Enemy.cpp
@@ -1,5 +1,9 @@
 #include "entities/enemies/Enemy.h"
 
+//Throwing this up here so it's consistent within the class
+const float PI = 3.1415927f;
+
 void Enemy::update() {
 	move(getAngle(player->getPosition()));
+	sprite.setRotation(getAngle(player->getPosition()) * 180 / PI + 90);
 }
